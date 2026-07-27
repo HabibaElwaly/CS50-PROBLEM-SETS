5b592b87194a76386ea571dac099482b392b6b0f
Habiba El-waly <habibamohamedheshmat@gmail.com>
2026-07-27 03:06:40 +0300

    Add files via upload

--- PSET2/readability.c
+++ PSET2/readability.c
@@ -1,1 +1,50 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
 
+int main(void)
+{
+    string text = get_string("Text: ");
+
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            letters++;
+        }
+        else if (text[i] == ' ' || i == 0)
+        {
+            words++;
+        }
+        else if (text[i] == '.' || text[i] == '?' || text[i] == '!')
+        {
+            sentences++;
+        }
+    }
+
+    float l = (float) letters / words * 100;
+    float s = (float) sentences / words * 100;
+
+    float index = 0.0588 * l - 0.296 * s - 15.8;
+
+    int grade = round(index);
+
+    if (grade >= 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
+}
